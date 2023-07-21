f55b3c9535933a2d743a81dbd6d465538124a447
zundokoberoncho <zunbero_zundokoberoncho@yahoo.co.jp>
2023-07-21 21:58:02 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
+  has_many :room_users
+  has_many :users, through: :room_users
 end
--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,7 @@
 class RoomUser < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
+  belongs_to :room
+  belongs_to :user
+
+  validates :name, presence: true
+
 end
