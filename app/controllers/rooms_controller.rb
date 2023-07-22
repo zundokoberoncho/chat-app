7f083bae1fc2d5e9e6fefb4a8e3fb10421f43338
zundokoberoncho <zunbero_zundokoberoncho@yahoo.co.jp>
2023-07-22 19:33:59 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,7 +9,7 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
-    if @room.save!
+    if @room.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,4 @@
 class RoomUser < ApplicationRecord
   belongs_to :room
   belongs_to :user
-
-  validates :name, presence: true
-
 end
